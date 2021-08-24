4850b3d0922a9a5d8fa4015d48d7e06eb63fc9ee
Jordi Funollet <funollet@fastmail.fm>
2021-08-24 19:52:48 +0200

    Finds the desired binary inside the downloaded file on its own.

---
+++ tests/test_guess.py
@@ -0,0 +1,80 @@
+import os
+import sys
+from contextlib import contextmanager
+from pathlib import Path
+
+import pytest
+
+from fetchbin.console import guess_bin_path
+
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+
+@pytest.mark.parametrize(
+    "fixture_dir,expected",
+    [
+        ("tests/fixtures/a/", "project"),
+        ("tests/fixtures/c/", "bin/project"),
+        ("tests/fixtures/d/", "bin/project"),
+        ("tests/fixtures/e/", "project/project"),
+        ("tests/fixtures/g/", "project-v0.0.0/project"),
+        ("tests/fixtures/h/", "bin/project"),
+        ("tests/fixtures/i/", "project-v0.0.0/bin/project"),
+        ("tests/fixtures/k/", "project-0.0.0/project"),
+        ("tests/fixtures/l/", "project-0.0.0/bin/project"),
+    ],
+)
+def test_guess_bin_path(fixture_dir, expected):
+    with cd(fixture_dir):
+        assert guess_bin_path("project") == Path(expected)
+
+
+@pytest.mark.parametrize(
+    "fixture_dir,expected",
+    [
+        ("tests/fixtures/n/", "project-darwin-amd64"),
+        ("tests/fixtures/o/", "project-darwin-amd64"),
+        ("tests/fixtures/p/", "project-v0.0.0-darwin"),
+        ("tests/fixtures/q/", "project-0.0.0-darwin"),
+    ],
+)
+@pytest.mark.skipif(sys.platform != "darwin", reason="Mac specific tests")
+def test_guess_bin_path_macos(fixture_dir, expected):
+    with cd(fixture_dir):
+        assert guess_bin_path("project") == Path(expected)
+
+
+@pytest.mark.parametrize(
+    "fixture_dir,expected",
+    [
+        ("tests/fixtures/b/", "project-linux-amd64"),
+        ("tests/fixtures/f/", "project-linux-amd64"),
+        ("tests/fixtures/j/", "project-v0.0.0-linux"),
+        ("tests/fixtures/m/", "project-0.0.0-linux"),
+    ],
+)
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux specific tests")
+def test_guess_bin_path_linux(fixture_dir, expected):
+    with cd(fixture_dir):
+        assert guess_bin_path("project") == Path(expected)
+
+
+@pytest.mark.parametrize(
+    "fixture_dir",
+    [
+        ("tests/fixtures/not-found-a/"),
+        ("tests/fixtures/not-found-b/"),
+    ],
+)
+def test_guess_bin_path_not_found(fixture_dir):
+    with pytest.raises(FileNotFoundError):
+        with cd(fixture_dir):
+            guess_bin_path("project")
